fd3ec9898c4c9424f473087199d75d2bb13a0459
Qiurui Liu <qiuruiliu@Qiuruis-MacBook-Pro.local>
2020-10-11 22:40:53 -0700

    first try

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -4,9 +4,13 @@ import HTMLText from "../HTMLText/HTMLText";
 const DynamicArticle = (props) => {
   return (
     <article>
-      {props.article.author}({props.article.authorEmail})
+      <header>
+      <h1>{props.article.title}</h1>
+      by {props.article.author}(<a href = "mailto:{props.article.authorEmail}">{props.article.authorEmail}</a>
       <time dateTime = {props.article.timeStamp}>{props.article.displayDate}</time>
-      <HTMLText text={props.article.text} />
+      </header>
+      
+      <p><HTMLText text={props.article.text} /></p>
     </article>
   );
 };
