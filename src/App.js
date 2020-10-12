4076dcd661f7a6834be3535abb0d75559f3ea658
lqr0717 <55065581+lqr0717@users.noreply.github.com>
2020-10-11 23:02:25 -0700

    Update App.js
    second try

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         
       );
       const responseJson = await response.json();
-      setFetchedData(responseJson.data);
+      setFetchedData(responseJson);
     };
 
     if (isEmpty(fetchedData)) {
